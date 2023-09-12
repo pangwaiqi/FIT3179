43d579db1e4ecc9284b6ed348be8aa3a994b2b77
Pang Wai Qi <wpan0021@student.monash.edu>
2023-09-12 14:53:08 +0800

    first commit

---
+++ Project 2/charts/vega_lite_vis.js
@@ -0,0 +1,3 @@
+vegaEmbed('#chart1', "charts/chart1.json").then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
