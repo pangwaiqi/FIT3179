184fa6591c39c592c77daba77741dadaf2efea86
pangwaiqi <wpan0021@student.monash.edu>
2023-09-12 22:44:23 +0800

    added line chart

--- Project 2/charts/vega_lite_vis.js
+++ Project 2/charts/vega_lite_vis.js
@@ -3,3 +3,5 @@ vegaEmbed('#chart1', "charts/chart1.json").then(function(result) {
 }).catch(console.error);
 vegaEmbed('#chart2', "charts/chart2.json").then(function(result) {
 }).catch(console.error);
+vegaEmbed('#chart3', "charts/chart3.json").then(function(result) {
+}).catch(console.error);
