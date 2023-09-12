72d7ca943d4f4addf178c73160a6564cade41f81
Pang Wai Qi <wpan0021@student.monash.edu>
2023-09-12 20:20:42 +0800

    added choropleth map and bar chart

--- Project 2/charts/vega_lite_vis.js
+++ Project 2/charts/vega_lite_vis.js
@@ -1,3 +1,5 @@
 vegaEmbed('#chart1', "charts/chart1.json").then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+vegaEmbed('#chart2', "charts/chart2.json").then(function(result) {
+}).catch(console.error);
