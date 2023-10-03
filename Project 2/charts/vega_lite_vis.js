e6e4b7c44b8cc776134fad3dbea1507d9577f02a
pangwaiqi <wpan0021@student.monash.edu>
2023-10-03 18:46:43 +0800

    added choropleth map for global suicide rate

--- Project 2/charts/vega_lite_vis.js
+++ Project 2/charts/vega_lite_vis.js
@@ -1,19 +1,21 @@
-vegaEmbed('#chart1', "charts/chart1.json").then(function(result) {
+vegaEmbed('#chart1', "charts/chart1.json").then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-vegaEmbed('#chart2', "charts/chart2.json").then(function(result) {
+vegaEmbed('#chart2', "charts/chart2.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart3', "charts/chart3.json").then(function(result) {
+vegaEmbed('#chart3', "charts/chart3.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart4', "charts/chart4.json").then(function(result) {
+vegaEmbed('#chart4', "charts/chart4.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart5', "charts/chart5.json").then(function(result) {
+vegaEmbed('#chart5', "charts/chart5.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart6', "charts/chart6.json").then(function(result) {
+vegaEmbed('#chart6', "charts/chart6.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart7', "charts/chart7.json").then(function(result) {
+vegaEmbed('#chart7', "charts/chart7.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart8', "charts/chart8.json").then(function(result) {
+vegaEmbed('#chart8', "charts/chart8.json").then(function (result) {
 }).catch(console.error);
-vegaEmbed('#chart9', "charts/chart9.json").then(function(result) {
+vegaEmbed('#chart9', "charts/chart9.json").then(function (result) {
+}).catch(console.error);
+vegaEmbed('#chart10', "charts/chart10.json").then(function (result) {
 }).catch(console.error);
